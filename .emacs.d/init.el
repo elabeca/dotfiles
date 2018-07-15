1a8739b3708f0ab77f27970857d11c4232a469b2
Elie Labeca <elie.labeca@googlemail.com>
2018-07-15 20:26:13 +0100

    Slight update to emacs init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,72 +1,78 @@
-;; global variables
-(setq
- inhibit-startup-screen t
- create-lockfiles nil
- make-backup-files nil
- column-number-mode t
- scroll-error-top-bottom t
- show-paren-delay 0.5
- use-package-always-ensure t
- sentence-end-double-space nil)
+;; init.el --- Emacs configuration
 
-;; buffer local variables
-(setq-default
- indent-tabs-mode nil
- tab-width 4
- c-basic-offset 4)
+;; INSTALL PACKAGES
+;; --------------------------------------
 
-;; modes
-(electric-indent-mode 0)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(global-auto-revert-mode 1)
-(scroll-bar-mode -1)
-
-;; global keybindings
-(global-unset-key (kbd "C-z"))
-
-;; the package manager
 (require 'package)
-(setq
- package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                    ("org" . "http://orgmode.org/elpa/")
-                    ("melpa" . "http://melpa.org/packages/")
-                    ("melpa-stable" . "http://stable.melpa.org/packages/"))
- package-archive-priorities '(("melpa-stable" . 1)))
-
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
 (when (not package-archive-contents)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
+  (package-refresh-contents))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" default)))
- '(package-selected-packages
-   (quote
-    (zoom-frm helm magit sublime-themes projectile ensime use-package))))
+ '(package-selected-packages (quote (helm smex magit cider ##))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(use-package ensime
-  :ensure t
-  :pin melpa-stable)
-(use-package projectile
-  :demand
-  :init   (setq projectile-use-git-grep t)
-  :config (projectile-global-mode t)
-  :bind   (("s-f" . projectile-find-file)
-           ("s-F" . projectile-grep)))
-;; themes
-(load-theme 'spolsky t)
+(defvar myPackages
+  '(better-defaults
+    ein
+    elpy
+    flycheck
+    material-theme
+    py-autopep8))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+;; BASIC CUSTOMIZATION
+;; --------------------------------------
+(setq inhibit-startup-message t
+      create-lockfiles nil
+      column-number-mode t
+      scroll-error-top-bottom t
+      show-paren-delay 0.5
+      use-package-always-ensure t
+      sentence-end-double-space nil)
+
+;; MODES
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+(load-theme 'material t) ;; load material theme
+(elpy-enable)
+(setq python-indent-offset 2
+      python-shell-interpreter "jupyter"
+      python-shell-interpreter-args "console --simple-prompt"
+      python-shell-prompt-detect-failure-warning nil)
+(add-to-list 'python-shell-completion-native-disabled-interpreters
+             "jupyter")
+
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
 ;; helm
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-m") 'helm-M-x)
@@ -75,12 +81,16 @@
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-=") 'zoom-frm-in)
 (global-set-key (kbd "C--") 'zoom-frm-out)
+
 ;; display time on status bar
  (setq display-time-day-and-date t
        display-time-24hr-format t)
        (display-time)
+
 ;; moving backup files to temp directory
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
-      `((".*" ,tembporary-file-directory t)))
+      `((".*" ,temporary-file-directory t)))
+
+;; init.el ends here
